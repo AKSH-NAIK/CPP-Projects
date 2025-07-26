cefff0be9800f80e912edf701d9b87474594d15d
AKSH NAIK <akshnaik1907@gmail.com>
2025-07-26 18:34:02 +0530

    Update calculator.cpp
    Adds validation for input and operator

--- Calculator/calculator.cpp
+++ Calculator/calculator.cpp
@@ -8,15 +8,29 @@ int main() {
     
     // First Input
     cout << "Enter first number: ";
-    cin >> num1;
+    while (!(cin >> num1)) {
+    cout << "Invalid input. Please enter a number: ";
+    cin.clear();
+    cin.ignore(10000, '\n');
+}
     
     //Ask for Operation
-    cout << "Enter operator (+, -, *, /): ";
+   cout << "Enter operator (+, -, *, /): ";
+  cin >> op;
+  while (op != '+' && op != '-' && op != '*' && op != '/') {
+    cout << "Invalid operator! Please enter one of (+, -, *, /): ";
     cin >> op;
+}
+
+    
     
     //Second Input
     cout << "Enter second number: ";
-    cin >> num2;
+      while (!(cin >> num2)) {
+    cout << "Invalid input. Please enter a number: ";
+    cin.clear();
+    cin.ignore(10000, '\n');
+}
 
     // Perform calculation based on operator
     switch(op) {
